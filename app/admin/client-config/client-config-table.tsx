c853a91a2b2cefeb5d9b6b5709c9e08611d83d69
rbnbrls <3826339+rbnbrls@users.noreply.github.com>
2026-08-02 09:46:59 +0200

    fix: add missing import for ClientConfigEditWizard in client-config-table

--- app/admin/client-config/client-config-table.tsx
+++ app/admin/client-config/client-config-table.tsx
@@ -13,6 +13,7 @@ import {
   getRowTintStyle,
 } from "@/lib/client-config-formatting";
 import { RetirePortfolioModal } from "./retire-portfolio-modal";
+import ClientConfigEditWizard from "./client-config-edit-wizard";
 import { canEditClientConfigRow } from "@/lib/client-config-edit-permission";
 
 type Row = ClientConfigPortfolioConfigurationRow;
